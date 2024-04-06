f1dffa7812076da87188108ddc275cacb9ec3b75
Raul Calvo <raul.cal.bar@gmail.com>
2024-04-06 12:37:23 +0200

    added config to connect db

--- src/env.js
+++ src/env.js
@@ -7,12 +7,11 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
+    POSTGRES_PRISMA_URL: z .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL"
+        (str) => !str.includes(" "),
+        "You forgot to change the default URL for database connection"
       ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -44,7 +43,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
