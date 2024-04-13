3ddfbcf95151ad7c6ec11394c4ea202d1a5705a9
Raul Calvo <raul.cal.bar@gmail.com>
2024-04-13 13:09:33 +0200

    feat: functional authentication

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,58 +6,71 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
-
-import { env } from "@/env";
+import CredentialsProvider from "next-auth/providers/credentials";
+import crypto from "@/server/lib/crypto";
 import { db } from "@/server/db";
+import { type User } from "@/server/lib/types";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
+
 declare module "next-auth" {
   interface Session extends DefaultSession {
-    user: DefaultSession["user"] & {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    };
+    user: DefaultSession["user"] & User
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
+  session:{
+    strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/users/login'
+  },
   callbacks: {
-    session: ({ session, user }) => ({
+    jwt: async ({token, user}) => {
+      user && (token.user = user)
+      return Promise.resolve(token)
+    },
+    session: ({ session, token }) => {
+      const user: User = token.user as User
+
+      return ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        ...user
       },
-    }),
+    })},
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    // Login with username and password
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Usuario',
+          type: 'text',
+          placeholder: 'usuarioPrueba'
+        },
+        password: { label: 'Contraseña', type: 'password', placeholder: 'pass123'}
+      },
+      async authorize(credentials){
+      // Find user in database
+      const dbUser = await db.user.findUnique({
+        where: {
+          username: credentials?.username
+        }
+      })
+
+      // If user is not found, return null
+      if (!dbUser) return null
+
+      // Checks if password is ok
+      const isPasswordOK = crypto().compareWithHash(credentials!.password, dbUser.password)
+
+      if (isPasswordOK) return dbUser
+      return null
+    }})
   ],
 };
 
