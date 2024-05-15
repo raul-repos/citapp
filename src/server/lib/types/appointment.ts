5e5b149a0569692d5cd65d0c0cff6648bfa54656
raul_calvo <raul.calvo@inforyde.com>
2024-05-15 08:14:17 +0200

    update appointment model with date

--- src/server/lib/types/appointment.ts
+++ src/server/lib/types/appointment.ts
@@ -4,6 +4,7 @@ export const Appointment = z.object({
     id: z.string(),
     userId: z.string(),
     patientId: z.string(),
+    date: z.date()
 })
 
 export type Appointment = z.infer<typeof Appointment>
